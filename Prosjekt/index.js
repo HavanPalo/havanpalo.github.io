64fce90545eea50629e8fdb5923c6224b4b72631
SveinIsdahl <300708.svisdahl@gmail.com>
2019-10-24 15:52:16 +0200

    javascrpit buttons virker

--- Prosjekt/index.js
+++ Prosjekt/index.js
@@ -1,34 +1,46 @@
+let om1 = document.getElementById("om1");
+let om2 = document.getElementById("om2");
+let om3 = document.getElementById("om3");
+om1.style.display = "none";
+om2.style.display = "none";
+om3.style.display = "none";
+
 function func1() {
-    let x = document.getElementById("om1");
-    if ( x.style.display === "none") {
-        x.style.display = "block";
+    if (om1.style.display === "none") {
+        om1.style.display = "block";
+        om2.style.display = "none";
+        om3.style.display = "none";
         console.log("1r3");
     }
     else {
-        x.style.display = "none";
+        om1.style.display = "none";
         console.log("egsg")
     }
-   
+
 }
+
 function func2() {
-    let x = document.getElementById("om2");
-    if ( x.style.display === "none") {
-        x.style.display = "block";
+    if (om2.style.display === "none") {
+        om3.style.display = "none";
+        om2.style.display = "block";
+        om1.style.display = "none";
         console.log("1r3");
     }
     else {
-        x.style.display = "none";
+        om2.style.display = "none";
         console.log("egsg")
     }
 }
+
 function func3() {
-    let x = document.getElementById("om3");
-    if ( x.style.display === "none") {
-        x.style.display = "block";
+    if (om3.style.display === "none") {
+        om3.style.display = "block";
+        om2.style.display = "none";
+        om1.style.display = "none";
         console.log("1r3");
     }
     else {
-        x.style.display = "none";
+        om3.style.display = "none";
         console.log("egsg")
     }
 }
