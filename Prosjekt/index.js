96f673014cbecfe90c99b61403e41658ffbd72cf
SveinIsdahl <300708.svisdahl@gmail.com>
2019-10-24 10:16:12 +0200

    Ikke ferdig med javascript

---
+++ Prosjekt/index.js
@@ -0,0 +1,34 @@
+function func1() {
+    let x = document.getElementById("om1");
+    if ( x.style.display === "none") {
+        x.style.display = "block";
+        console.log("1r3");
+    }
+    else {
+        x.style.display = "none";
+        console.log("egsg")
+    }
+   
+}
+function func2() {
+    let x = document.getElementById("om2");
+    if ( x.style.display === "none") {
+        x.style.display = "block";
+        console.log("1r3");
+    }
+    else {
+        x.style.display = "none";
+        console.log("egsg")
+    }
+}
+function func3() {
+    let x = document.getElementById("om3");
+    if ( x.style.display === "none") {
+        x.style.display = "block";
+        console.log("1r3");
+    }
+    else {
+        x.style.display = "none";
+        console.log("egsg")
+    }
+}
